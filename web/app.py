d2eef7a79ac1eacfea28f27767e8d62a1c0b89af
Marcos <marcosnery.comp@gmail.com>
2021-05-22 02:39:52 -0300

    feat: setting up file upload

--- web/app.py
+++ web/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 import requests
+import os
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+UPLOAD_FOLDER = './upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        return path
+
+        return 'ok'
 
 
 @app.route('/')
@@ -12,6 +31,7 @@ def index():
   return render_template('index.html')
 
 
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
+
+
